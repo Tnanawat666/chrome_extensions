4fa63a9d83342d2ab6161eb7faafe6c06aedc8d8
thinnawarth mathuros <thinnawarth@gmail.com>
2024-05-08 10:43:15 +0700

    first commit

---
+++ background.js
@@ -0,0 +1,24 @@
+chrome.runtime.onInstalled.addListener(() => {
+    chrome.storage.local.set({
+        name:"Jack"
+    });
+});
+
+chrome.storage.local.get("name", data => {
+
+});
+
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+    
+     
+    if(changeInfo.status === "complete" && tab.url.includes("https://astronize.com")) {
+        chrome.scripting.executeScript({
+            target: {tabId: tabId},
+            files: ["./foreground.js"]
+        })
+        .then(() => {
+            console.log("injected")
+        })
+        .catch(err => console.log(err))
+    }
+})
