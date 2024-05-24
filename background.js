836676a841f553ffd5dca5ecf21c055d02f53e75
Boom <119836646+Tnanawat666@users.noreply.github.com>
2024-05-24 10:45:08 +0700

    [Test]: Inventory Testing

--- background.js
+++ background.js
@@ -6,11 +6,17 @@ chrome.storage.local.get("name", (data) => {});
 
 const enURL = "https://astronize.com/market";
 const thURL = "https://astronize.com/th/market";
+const bagEN = "https://astronize.com/market/inventory";
+const bagTH = "https://astronize.com/th/market/inventory";
 const injectedTabs = new Set();
 
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
   if (changeInfo.status === "complete") {
-    if (tab.url && (tab.url.includes(enURL) || tab.url.includes(thURL))) {
+    if (
+      (tab.url && (tab.url.includes(enURL) || tab.url.includes(thURL))) ||
+      tab.url.includes(bagEN) ||
+      tab.url.includes(bagTH)
+    ) {
       if (!injectedTabs.has(tabId)) {
         chrome.scripting
           .executeScript({
@@ -28,7 +34,10 @@ chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
 
   if (
     changeInfo.url &&
-    (changeInfo.url.includes(enURL) || changeInfo.url.includes(thURL))
+    (changeInfo.url.includes(enURL) ||
+      changeInfo.url.includes(thURL) ||
+      changeInfo.url.includes(bagEN) ||
+      changeInfo.url.includes(bagTH))
   ) {
     chrome.tabs.reload(tabId);
   }
